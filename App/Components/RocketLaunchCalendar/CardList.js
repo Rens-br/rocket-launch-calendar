0cb6b97b325198ca6fd45343863de33611ec9c61
Rens-br <rensbrekel@gmail.com>
2018-12-27 16:42:10 +0100

    Add rounding to top corners of news cards

--- App/Components/RocketLaunchCalendar/CardList.js
+++ App/Components/RocketLaunchCalendar/CardList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { FlatList } from 'react-native'
-import { Card, Paragraph, Title } from 'react-native-paper'
+import { Card, Paragraph, Title, DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
 import NavigationService from '../../Services/NavigationService'
 import Colors from '../../Theme/Colors'
 
@@ -26,26 +26,28 @@ export default class CardList extends Component {
           keyExtractor={(item) => item.data._id}
           renderItem={({ item }) => {
             return (
-              <Card
-                onPress={() => {
-                  NavigationService.navigate('WebViewScreen', {
-                    url: item.data.url,
-                    title: item.data.title,
-                    source: item.data.news_site_long,
-                  })
-                }}
-                style={{ borderRadius: 8, width: '90%', alignSelf: 'center', marginTop: 20 }}
-              >
-                <Card.Cover source={{ uri: item.data.featured_image }} />
-                <Card.Content>
-                  <Title style={{ fontSize: 22, lineHeight: 24, marginTop: 10 }}>
-                    {item.data.title}
-                  </Title>
-                  <Paragraph style={{ color: Colors.disabledText }}>
-                    {item.data.news_site_long}
-                  </Paragraph>
-                </Card.Content>
-              </Card>
+              <PaperProvider theme={theme}>
+                <Card
+                  onPress={() => {
+                    NavigationService.navigate('WebViewScreen', {
+                      url: item.data.url,
+                      title: item.data.title,
+                      source: item.data.news_site_long,
+                    })
+                  }}
+                  style={{ borderRadius: 12, width: '90%', alignSelf: 'center', marginTop: 20 }}
+                >
+                  <Card.Cover source={{ uri: item.data.featured_image }} />
+                  <Card.Content>
+                    <Title style={{ fontSize: 22, lineHeight: 24, marginTop: 10 }}>
+                      {item.data.title}
+                    </Title>
+                    <Paragraph style={{ color: Colors.disabledText }}>
+                      {item.data.news_site_long}
+                    </Paragraph>
+                  </Card.Content>
+                </Card>
+              </PaperProvider>
             )
           }}
           onRefresh={this.props.refresh}
@@ -56,4 +58,14 @@ export default class CardList extends Component {
       )
     }
   }
+}
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 12,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
 }
