7a3d6a7cdb12aabcd1438a4f5dbca4e75e1aeaa7
Rens-br <rensbrekel@gmail.com>
2019-01-16 12:29:37 +0100

    Fix news filter duplicating news post

--- App/Sagas/SpaceFlightNewsSaga.js
+++ App/Sagas/SpaceFlightNewsSaga.js
@@ -7,58 +7,38 @@ export function* fetchNews(params) {
   const News = yield call(SpaceFlightNewsService.fetchNews, params.Page)
 
   if (News) {
-    let checkedNews = []
+    let dirtyNews = [].concat(News)
 
     for (let i = 0; i < News.length; i++) {
       if (
-        params.BlockedSources !== null &&
         params.BlockedSources !== undefined &&
-        params.BlockedSources.length
+        params.BlockedSources.length &&
+        News[i].news_site !== undefined
       ) {
         for (let j = 0; j < params.BlockedSources.length; j++) {
-          if (News[i] !== undefined) {
-            if (
-              News[i].news_site !== undefined &&
-              News[i].news_site.toLowerCase() !== params.BlockedSources[j].toLowerCase()
-            ) {
-              if (
-                params.BlockedTags !== null &&
-                params.BlockedTags !== undefined &&
-                params.BlockedTags.length
-              ) {
-                for (let j = 0; j < params.BlockedTags.length; j++) {
-                  if (News[i] !== undefined) {
-                    if (!News[i].tags.includes(params.BlockedTags[j].toLowerCase())) {
-                      checkedNews.push(News[i])
-                    }
-                  }
-                }
-              } else {
-                checkedNews.push(News[i])
-              }
+          if (News[i].news_site.toLowerCase() === params.BlockedSources[j].toLowerCase()) {
+            if (dirtyNews.includes(News[i])) {
+              dirtyNews.splice(dirtyNews.indexOf(News[i], 1))
             }
           }
         }
-      } else {
-        if (
-          params.BlockedTags !== null &&
-          params.BlockedTags !== undefined &&
-          params.BlockedTags.length
-        ) {
-          for (let j = 0; j < params.BlockedTags.length; j++) {
-            if (News[i] !== undefined) {
-              if (!News[i].tags.includes(params.BlockedTags[j].toLowerCase())) {
-                checkedNews.push(News[i])
-              }
+      }
+      if (
+        params.BlockedTags !== undefined &&
+        params.BlockedTags.length &&
+        News[i].tags !== undefined
+      ) {
+        for (let k = 0; k < params.BlockedTags.length; k++) {
+          if (News[i].tags.includes(params.BlockedTags[k].toLowerCase())) {
+            if (dirtyNews.includes(News[i])) {
+              dirtyNews.splice(dirtyNews.indexOf(News[i], 1))
             }
           }
-        } else {
-          checkedNews.push(News[i])
         }
       }
     }
 
-    yield put(SpaceFlightNewsActions.fetchNewsSuccess({ News: checkedNews, Page: params.Page }))
+    yield put(SpaceFlightNewsActions.fetchNewsSuccess({ News: dirtyNews, Page: params.Page }))
   } else {
     console.log('Error fetching news api')
   }
